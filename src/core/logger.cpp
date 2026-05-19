95cfb14e0fd395ff39fb645663302bf23365da38
theaveasso <theaveas.so@gmail.com>
2026-05-19 07:43:13 +0700

    chore: update logger code

--- src/core/logger.cpp
+++ src/core/logger.cpp
@@ -14,7 +14,7 @@ bool logger_init() {
 	auto rotate_sink  = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("logs/rtgj.log", logger_max_file_size, logger_max_files);
 
 	g_logger = std::make_shared<spdlog::logger>("rtgj_logger", spdlog::sinks_init_list{console_sink, rotate_sink});
-#ifndef NDEBUG
+#ifdef NDEBUG
 	g_logger->set_level(spdlog::level::trace);
 #else
 	g_logger->set_level(spdlog::level::info);
--- src/core/logger.hpp
+++ src/core/logger.hpp
@@ -11,3 +11,24 @@ void logger_shutdown();
 #define RTGJ_LOG_WARN(...)     SPDLOG_WARN(__VA_ARGS__)
 #define RTGJ_LOG_ERROR(...)    SPDLOG_ERROR(__VA_ARGS__)
 #define RTGJ_LOG_CRITICAL(...) SPDLOG_CRITICAL(__VA_ARGS__)
+
+#ifdef NDEBUG
+#define RTGJ_ASSERT(condition, message)
+do {
+	if (!(condition)) {
+		throw std::runtime_error(message);
+	}
+} while (false)
+#else
+#define ASSERT(condition, message) assert((condition) && (message))
+#endif
+
+    // #define VK_CHECK(vk_func) \
+// 	do {
+    //     if (const VkResult check_result = (vk_func); check_result != VK_SUCCESS) {
+    // 	const char* err = string_VkResult(check_result);
+    // 	RTGJ_LOG_ERROR(...);
+    // 	RTGJ_ASSERT(check_result == VK_SUCCESS, err);
+    // }
+    // }
+    // while (0)
